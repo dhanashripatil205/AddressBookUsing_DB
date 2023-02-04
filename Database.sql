2c49c9ee0e8bf749fb00c00133243bc36d2be8ee
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-05 01:23:25 +0530

    Retrieve Person Using City Or State!!!

---
+++ Database.sql
@@ -0,0 +1,28 @@
+create database AddressBookService
+create table AddressBook
+(
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+zip varchar(255),
+PhoneNum varchar(255),
+Email varchar(255)
+)
+
+insert into AddressBook values('Dhanshri','Patil','51 street','Mumbai','MH','369852','9456863255','dhanashri@gmail.com')
+insert into AddressBook values('Kriya','Sawant','52 area','banglore','MH','789654','6598763255','kriya@gmail.com')
+insert into AddressBook values('Kriti','Sawant','639','Pune','MH','236598','95683256988','kriti@gmail.com')
+insert into AddressBook values('Ganesh','patil','369 street','Hydrabad','MH','158963','9402569855','ganesh@gmail.com')
+
+update AddressBook
+set State='MH'
+where FirstName='Dhanshri'
+
+
+delete from AddressBook
+where FirstName='Kriti'
+
+select * from AddressBook
+where City='Mumbai' or State='MH'
